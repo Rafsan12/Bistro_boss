492baac28dddee4c4e405c998be74f546abe1ecb
Rafsan12 <rafsan.web.2022@gmail.com>
2025-01-09 11:53:23 +0600

    menu part complete

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
+import MenuItem from "../../../components/MenuItem/MenuItem";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 export default function PopularMenu() {
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    const fetchMenuData = async () => {
+      const response = await fetch("menu.json");
+      const result = await response.json();
+      const popularMenu = result.filter((item) => item.category === "popular");
+      setMenu(popularMenu);
+    };
+    fetchMenuData();
+  }, []);
   return (
-    <>
+    <div className="mb-12">
       <SectionTitle heading={"FROM OUR MENU"} subHeading={"Check it out"} />
-    </>
+      <div className="grid md:grid-cols-2 gap-12">
+        {menu.map((item) => (
+          <MenuItem key={item._id} item={item} />
+        ))}
+      </div>
+    </div>
   );
 }
